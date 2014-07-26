9c1aec05c6165190de03f40e7b6a7483dcaf93dc
Blake Mason <blnktd@gmail.com>
2014-07-26 12:53:39 -0700

    Adds jquery

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,36 @@ var fiveLetterWordGame = function(secretWord, guessWord) {
   		}
   	})
   })
-  console.log(score);
   return score;
 }
 
 $(document).ready(function() {
-	
+	var guessWords;
+	var yourSecretWord;
+	var yourGuessWord;
+  $("form#secreting").submit(function(event) {
+	yourSecretWord = $("input#secret-word").val();
+  	// alert(yourSecretWord);
+	$("form#secreting").hide();
+	$("form#guessing").show();
+	event.preventDefault();
+  })
+  $("form#guessing").submit(function() {
+    yourGuessWord = $("input#guess-word").val();
+	var yourScore = fiveLetterWordGame(yourSecretWord, yourGuessWord);
+	$("#guesses").show();
+	alert(yourScore);
+	$(".current-guess").text(yourGuessWord.toUpperCase());
+	$(".current-score").text(yourScore);
+    $("ul#guessed-words").append("<li>" + yourGuessWord.toUpperCase() + " : " + yourScore + "</li>");
+    $("input#guess-word").val("");
+    if (yourGuessWord === yourSecretWord) {
+      $("form#guessing").hide();
+      $("#win-message").show();
+      $(".current-secret-word").text(yourSecretWord);
+    }
+  event.preventDefault();
+  })
+})
+
+// <li><span class="past-guesses"></span> : <span class="pastScores"></span></li>
